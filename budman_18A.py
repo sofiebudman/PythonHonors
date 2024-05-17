d84780cf07a38f1036eee4a32f7ac1b35aecf64c
Sofie Budman <105175854+sofiebudman@users.noreply.github.com>
2024-05-17 09:05:15 -0700

    Add files via upload

---
+++ budman_18A.py
@@ -0,0 +1,35 @@
+#problem 1
+#choice =input('Enter y/n: ')
+#if(choice.lower() == 'y'):
+    #print('The choice was yes.')
+#problem 2
+#stringInput = 'this is a very long string'
+#numSpaces = 0
+#for i in range(len(stringInput)):
+ #   if(stringInput[i] == ' '):
+  #      numSpaces +=1
+#print(f'There are {numSpaces} spaces in "{stringInput}".')
+#problem 3
+
+#three = 'this1is2a3very187long4string'
+#digits = 0
+#for i in range(len(three)):
+#    if(three[i].isdigit()):
+#        digits +=1
+#print(f'"{three}" has {digits} digits. ')
+
+#problem 4
+#four = 'HuRrAy'
+#lowercase = 0
+#for i in range(len(four)):
+#    if(four[i].islower()):
+#        lowercase+=1
+#print(f'"{four}" has {lowercase} lowercase characters.')
+        
+
+#problem 5
+def check(string):
+    if(string.endswith('.com'):
+       return True
+    else:
+        return False
